2c198a1bb2bb37fba68c7f8526c5b14dcfc09ac3
Betroxen <encryptedcryptowallet@gmail.com>
2025-11-19 16:29:36 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,20 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AppProvider } from './context/AppContext';
-import { ToastProvider } from './context/ToastContext';
+import { Toaster } from './context/ToastContext';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppProvider>
-      <ToastProvider>
+      <Toaster>
         <App />
-      </ToastProvider>
+      </Toaster>
     </AppProvider>
   </React.StrictMode>
 );
